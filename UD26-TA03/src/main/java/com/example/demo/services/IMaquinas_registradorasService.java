4a81ff3ec46fd76f053a66bb3d6cac23c935aa81
Ainara <ainarabliz@gmail.com>
2023-11-14 02:25:42 +0100

    controllers hechos

--- UD26-TA03/src/main/java/com/example/demo/services/IMaquinas_registradorasService.java
+++ UD26-TA03/src/main/java/com/example/demo/services/IMaquinas_registradorasService.java
@@ -11,9 +11,9 @@ public interface IMaquinas_registradorasService {
 	
 	public Maquinas_registradoras guardar(Maquinas_registradoras maquinas_registradoras);	//Guarda 
 	
-	public Maquinas_registradoras porIdentificador(int id); //Leer datos 
+	public Maquinas_registradoras porIdentificador(int codigo_maquina); //Leer datos 
 	
 	public Maquinas_registradoras actualizar(Maquinas_registradoras maquinas_registradoras); //Actualiza datos 
 	
-	public void eliminar(int id);// Elimina datos
+	public void eliminar(int codigo_maquina);// Elimina datos
 }
